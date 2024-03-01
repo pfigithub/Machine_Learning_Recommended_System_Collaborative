473142aa65ddf4c5d0c1f032daa8abd6c4148822
pfigithub <pampam7072@gmail.com>
2024-03-01 10:53:03 +0330

    pre-processing

--- collaborative.py
+++ collaborative.py
@@ -11,3 +11,20 @@ import matplotlib.pyplot as plt
 movies_df = pd.read_csv('movies.csv')
 #Storing the user information into a pandas dataframe
 ratings_df = pd.read_csv('ratings.csv')
+
+#Using regular expressions to find a year stored between parentheses
+#We specify the parantheses so we don't conflict with movies that have years in their titles
+movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))',expand=False)
+#Removing the parentheses
+movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
+#Removing the years from the 'title' column
+movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '')
+#Applying the strip function to get rid of any ending whitespace characters that may have appeared
+movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
+
+#Dropping the genres column
+movies_df = movies_df.drop('genres', 1)
+
+#Drop removes a specified row or column from a dataframe
+ratings_df = ratings_df.drop('timestamp', 1)
+
