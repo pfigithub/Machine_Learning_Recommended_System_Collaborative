615ea4612713845ace6b61fac07dcf0f51623521
pfigithub <pampam7072@gmail.com>
2024-03-01 10:49:49 +0330

    import

---
+++ collaborative.py
@@ -0,0 +1,6 @@
+#Dataframe manipulation library
+import pandas as pd
+#Math functions, we'll only need the sqrt function so let's import only that
+from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
